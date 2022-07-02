d978dad1eb1bd86b37cb2daa2a7217d6c5bcd212
Nique Alisson Pereira de Souza <alisson_201083@hotmail.com>
2022-07-01 22:54:28 -0300

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Counter } from './components/Counter';
+import { RepositoryList } from './components/RepositoryList';
 import './styles/global.scss';
 
 
 export function App() {
 
-    return <h1>ReactJs</h1>
+    return (
+        <>
+    <RepositoryList />
+    <Counter />
+    </>
+)
 }
---
+++ src/components/RepositoryItem.jsx
@@ -0,0 +1,12 @@
+export function RepositoryItem(props) {
+    return (
+        <li>
+        <strong>{props.repository.name ?? 'default' }</strong>
+        <p>{props.repository.description ?? 'default'}</p>
+
+        <a href={props.repository.link}>
+            Acessar Repositorio
+        </a>
+    </li>
+    );
+}
